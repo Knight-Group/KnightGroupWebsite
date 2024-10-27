0506a19fd143edb99e9dfcac246a778f3d5a9b2b
Nicholasjknight <nicholas.jknight@yahoo.com>
2024-10-27 15:15:33 -0400

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,4 @@
+// Slideshow Functionality
 const slides = document.querySelectorAll('.slide');
 let currentSlide = 0;
 
@@ -22,7 +23,16 @@ function nextSlide() {
 // Automatically switch slides every 5 seconds
 setInterval(nextSlide, 5000);
 
+// Mobile Menu Toggle
 function toggleMenu() {
     var mobileMenu = document.querySelector('.mobile-menu');
     mobileMenu.classList.toggle('active');
 }
+
+// 404 Redirect to Homepage
+// Check if the current URL contains "404" or if the 404 page has loaded
+window.addEventListener('load', function() {
+    if (document.title.includes("404")) {
+        window.location.href = "https://KnightGroup.com";
+    }
+});
